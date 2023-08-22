35531b2c40c72fa5cfc3a5f6df26211fe15a5010
Hirusha1-cpu <hirushafernando121@gmail.com>
2023-08-22 08:19:26 +0530

    updatedda

--- src/page/Orders.js
+++ src/page/Orders.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-const Orders = () => {
+const Orders = ({ cartItems1 }) => {
     const productCartItem = useSelector((state) => state.product.cartItem)
     console.log("product cart orders page items ====>", productCartItem);
 
@@ -21,13 +21,30 @@ const Orders = () => {
     useEffect(() => {
         getCartItems();
     }, []);
+    const renderOrderItem = (item) => {
+        if (item.hasOwnProperty('qty' && 'name' && 'user')) {
+            return (
+                <div className="border  bg-white flex flex-row justify-between mt-4 gap-3  border-slate-900 p-3">
+                    <h2>{item.name}</h2>
+                    <p>{item.user}</p>
+                    <p>{item.qty}</p>
+                </div>
+            );
+        } else {
+            return null;
+        }
+    };
+
 
     return (
         <div>
             <h2 className='text-bold font-bold mt-2  text-4xl'>Orders</h2>
 
             <div className='mt-10 pr-5 w-auto flex gap-6 mb-4  flex-col ml-11'>
-                {cartItems.map((item, index) => {
+
+            {cartItems.map(renderOrderItem)}
+
+                {/* { cartItems.map((item, index) => {
                     return (
                         <div key={index} className='border  bg-white flex flex-row justify-between mt-4 gap-3  border-slate-900 p-3'>
                             
@@ -39,7 +56,8 @@ const Orders = () => {
 
                         </div>
                     )
-                })}
+                })} */}
+              
 
             </div>
         </div>
